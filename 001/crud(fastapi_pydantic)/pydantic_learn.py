6d4e2044fe2372b942ad44a080c97bf3ce18ffae
Akash Yadav <akashyadav2927@gmail.com>
2025-12-09 06:17:02 +0530

    summary pydantic example

--- 001/crud(fastapi_pydantic)/pydantic_learn.py
+++ 001/crud(fastapi_pydantic)/pydantic_learn.py
@@ -36,3 +36,33 @@ try:
 except Exception as e:
     print("ERROR:",e)
 
+# In this example, we define a User model with fields for username, age, email, is_active, and skills. We then simulate incoming data from an external source and use Pydantic to validate and parse that data into a User instance. If the data is valid, we print the user information; otherwise, we catch and print any validation errors.
+# 4 Testing with Invalid Data
+invalid_data={
+    "username":"arch_user",
+    "age":150,  # Invalid age
+    "email":"not-an-email",
+    "skills":["python","linux"]
+}
+try:
+    user=User(**invalid_data)
+    print("SUCCESS:")
+    print(user.model_dump())
+except Exception as e:
+    print("ERROR:",e)
+# In this case, the age is out of the specified range and the email is not valid, so Pydantic will raise validation errors, which we catch and print.
+# Overall, Pydantic provides a powerful way to ensure data integrity and consistency in Python applications
+# Example Output:
+# SUCCESS:
+# {'username': 'arch_user', 'age': 25, 'email': 'user
+#@example.com', 'is_active': True, 'skills': ['python', 'linux']}
+# user email:
+#
+# worong example output:
+# ERROR: 1 validation error for User
+# age
+#   ensure this value is less than 100 (type=value_error.number.not_lt; limit_value=100)
+# email
+#   value is not a valid email address (type=value_error.email)@example.com"
+# is_active: True
+# skills: ['python', 'linux']
